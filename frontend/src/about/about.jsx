f78eb54cd356857a7e3b11d8d14495948de0759d
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-08 22:29:08 -0200

    Basic React routes

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PageHeader from '../template/page-header'
+
+export default props => (
+  <div>
+    <PageHeader name='About' subname='Us'></PageHeader>
+  </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PageHeader from '../template/page-header'
+
+export default class Todo extends Component {
+
+  render() {
+    return (
+      <div>
+        <PageHeader name='Tasks' subname='Register'></PageHeader>
+      </div>
+    )
+  }
+}
