c281e2654d62b8ac781fcd1405ad68a97c1b82f4
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-09 18:41:44 -0200

    Layout changes

--- frontend/src/todo/todo-list.jsx
+++ frontend/src/todo/todo-list.jsx
@@ -10,21 +10,23 @@ export default props => {
       <tr key={todo._id} className='tasks__task'>
         <td className={todo.done ? 'tasks__task--done' : ''}>{todo.description}</td>
         <td className='tasks__task--actions'>
-          <IconButton
-            style='success'
-            icon='check'
-            hide={todo.done}
-            onClick={() => props.handleMarkAsDone(todo)}></IconButton>
-          <IconButton
-            style='warning'
-            icon='undo'
-            hide={!todo.done}
-            onClick={() => props.handleMaskAsPending(todo)}></IconButton>
-          <IconButton
-            style='danger'
-            icon='trash-o'
-            hide={!todo.done}
-            onClick={() => props.handleRemove(todo)}></IconButton>
+          <div className='btn-group'>
+            <IconButton
+              style='danger'
+              icon='times'
+              hide={!todo.done}
+              onClick={() => props.handleRemove(todo)}></IconButton>
+            <IconButton
+              style='success'
+              icon='check'
+              hide={todo.done}
+              onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+            <IconButton
+              style='warning'
+              icon='undo'
+              hide={!todo.done}
+              onClick={() => props.handleMaskAsPending(todo)}></IconButton>
+          </div>
         </td>
       </tr>
     ))
