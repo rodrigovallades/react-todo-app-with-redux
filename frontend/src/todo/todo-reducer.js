35c450ebdb04aa278cd84bcae7e69d561a195607
rodrigovallades <rodrigo.vallades@gmail.com>
2018-02-10 16:24:31 -0200

    Adding redux-promise as middleWare (now action creators can do ASYNC)

--- frontend/src/todo/todo-reducer.js
+++ frontend/src/todo/todo-reducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-  description: 'Read book',
-  list: [{
-    _id: 1,
-    description: 'Pay card',
-    done: true
-  }, {
-    _id: 2,
-    description: 'Aew',
-    done: false
-  }, {
-    _id: 3,
-    description: 'Medic',
-    done: true
-  }]
+  description: '',
+  list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
